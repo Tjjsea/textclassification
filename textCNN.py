c1695bd6250ccc6bac4397655486bb3615237842
TSEA <2694603918@qq.com>
2019-08-15 18:31:02 +0800

    textCNN

--- textCNN.py
+++ textCNN.py
@@ -6,3 +6,6 @@ import tensorflow as tf
 class tcModel():
     def __init__(self,flags):
         self.input_x=tf.placeholder(tf.int32,[None,None],name="input_x")
+        self.input_y=tf.placeholder(tf.int32,[None,flags.num_label],name="input_y")
+
+        
